592578bcf952da679fae6eadb9abfaf17db42c19
mengqian02 <mengqian02@gmail.com>
2016-03-09 18:04:50 +0800

    add page view logic

---
+++ app/views/Login.js
@@ -0,0 +1,56 @@
+/**
+ * mactive@gmail.com
+ * 2016-03-09
+ */
+
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions
+} from 'react-native';
+
+const {
+  width,
+  height
+} = Dimensions.get('window')
+
+
+
+class Login extends Component {
+  // 构造
+  constructor(props) {
+    super(props);
+    // 初始状态 
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Login View
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = Stylesheet.create({
+  container:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  title:{
+    fontSize: 20,
+    height:80,
+    widht: width/2,
+    textAlign: 'center'
+  }
+})
+
+module.exports = Login;
--- index.ios.js
+++ index.ios.js
@@ -3,29 +3,75 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React, {
+import React from 'react-native'
+import Profile from './app/views/Profile'
+import RefreshList from './app/view/RefreshList'
+import Login from './app/views/Login'
+
+const {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Navigator,
+  Platform,
+  TabBarIOS,
+  TouchableHighlight
+} = React;
 
 class RNNavigatorDemo extends Component {
-  render() {
+  // 构造
+  constructor(props) {
+    super(props);
+    // 初始状态
+    this.state = {
+      selectedTab: 'redTab',
+      notifCount: 2
+    };
+  }
+
+  _renderContent(_color: string, pageText: string){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}> need to login to view more</Text>
+        <TouchableHighlight style={[styles.button,{backgroundColor:_color}]} onPress={() => this.props.navigator.push({id: 'test'})}>
+          <Text>Login</Text>
+        </TouchableHighlight>
       </View>
+    )
+  }
+
+  render() {
+    return (
+      <TabBarIOS
+        tintColor="white"
+        barTintColor="#1565C0">
+        <TabBarIOS.Item
+          title="List"
+          systemIcon="search"
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          {this._renderContent('#2196F3', 'Blue Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="Profile"
+          badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+              notifCount: this.state.notifCount + 1,
+            });
+          }}>
+          {this._renderContent('#F44336', 'Need login')}
+        </TabBarIOS.Item>
+      </TabBarIOS>
     );
   }
 }
@@ -44,9 +90,14 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#000000',
     marginBottom: 5,
   },
+  button: {
+    backgroundColor: 'red',
+    padding: 10,
+    margin: 20
+  }
 });
 
 AppRegistry.registerComponent('RNNavigatorDemo', () => RNNavigatorDemo);
